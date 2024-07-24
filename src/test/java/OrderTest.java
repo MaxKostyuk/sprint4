0035334c5c920c06cbfb4a4f284cf92e3a9f7237
Kostyuk Max <kostyuk.max@gmail.com>
2024-07-24 17:01:15 +0800

    feat: parametrization added to OrderTest

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -5,10 +5,36 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 
+@RunWith(Parameterized.class)
 public class OrderTest {
     private WebDriver driver;
+    private final String name;
+    private final String lastName;
+    private final String address;
+    private final int stationNumber;
+    private final String phoneNumber;
+    private final int daysForRent;
+
+    public OrderTest(String name, String lastName, String address, int stationNumber, String phoneNumber, int daysForRent) {
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.stationNumber = stationNumber;
+        this.phoneNumber = phoneNumber;
+        this.daysForRent = daysForRent;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getOrderData() {
+        return new Object[][]{
+                {"Иван", "Кравцов", "Адрес", 5, "81234567890", 7},
+                {"Михаил", "Иванов", "Адрес2", 3, "80987654321", 1},
+        };
+    }
 
     @Before
     public void setUp() {
@@ -21,19 +47,18 @@ public class OrderTest {
         mainPage.openPage();
         mainPage.clickOrderButton();
         OrderPage orderPage = new OrderPage(driver);
-        orderPage.inputName("Иван");
-        orderPage.inputLastName("Кравцов");
-        orderPage.inputAddress("Примерный адрес");
-        orderPage.chooseSubwayStation(5);
-        orderPage.inputPhoneNumber("89991234567");
+        orderPage.inputName(name);
+        orderPage.inputLastName(lastName);
+        orderPage.inputAddress(address);
+        orderPage.chooseSubwayStation(stationNumber);
+        orderPage.inputPhoneNumber(phoneNumber);
         orderPage.clickButtonNext();
         orderPage.chooseFirstAvailableDate();
-        orderPage.chooseDaysForRent(7);
+        orderPage.chooseDaysForRent(daysForRent);
         orderPage.chooseGreyColor();
         orderPage.orderScooter();
         orderPage.confirmOrder();
         Assert.assertTrue(orderPage.checkOrderConfirmed());
-        System.out.println("wait");
     }
 
     @After
